2fe7a805ac8121c0c2f1f3d726e47da31496ce3b
AasharWahla <aiwahla.official@gmail.com>
2020-06-06 22:15:54 +0500

    Completed sign in and register screen without functionality.

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -7,8 +7,174 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  String name;
+  String email;
+  String phoneNumber;
+  String password;
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFFD0F1D7),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Center(
+          child: Text(
+            'Register Screen',
+            style: TextStyle(
+              fontFamily: 'HKGrotesk',
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+            ),
+          ),
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  validator: (val) {
+                    if (val.isEmpty) {
+                      return "This Field can not be empty.";
+                    } else if (val.length < 6) {
+                      return "Minimum Length is six";
+                    } else {
+                      return null;
+                    }
+                  },
+                  autocorrect: false,
+                  onChanged: (val) {
+                    name = val;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Name",
+                    icon: Icon(Icons.account_circle),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (val) {
+                    if (val.isEmpty) {
+                      return "This Field can not be empty.";
+                    } else if (val.length < 6) {
+                      return "Minimum Length is six";
+                    } else {
+                      return null;
+                    }
+                  },
+                  autocorrect: false,
+                  onChanged: (val) {
+                    phoneNumber = val;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Mobile # ",
+                    icon: Icon(Icons.phone),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (val) {
+                    if (val.isEmpty) {
+                      return "This Field can not be empty.";
+                    } else if (!val.contains("@")) {
+                      return "Invalid Email";
+                    } else {
+                      return null;
+                    }
+                  },
+                  autocorrect: false,
+                  onChanged: (val) {
+                    email = val;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    icon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  validator: (val) {
+                    if (val.isEmpty) {
+                      return "This Field can not be empty.";
+                    } else if (val.length < 6) {
+                      return "Minimum Length is six";
+                    } else {
+                      return null;
+                    }
+                  },
+                  obscureText: true,
+                  autocorrect: false,
+                  onChanged: (val) {
+                    password = val;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    icon: Icon(Icons.vpn_key),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                RaisedButton(
+//                  focusColor: Colors.blue,
+                  splashColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  elevation: 20,
+                  color: Colors.white,
+                  onPressed: () {},
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                        fontFamily: 'HKGrotesk',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/singIn.dart
+++ lib/screens/singIn.dart
@@ -39,72 +39,84 @@ class _SingInState extends State<SingIn> {
             key: _formKey,
             child: Container(
               width: MediaQuery.of(context).size.width * .8,
-              child: Column(
-                children: [
-                  TextFormField(
-                    validator: (val) {
-                      if (val.isEmpty) {
-                        return "This Field Can not be empty!";
-                      } else {
-                        return null;
-                      }
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (val) {
+                        if (val.isEmpty) {
+                          return "This Field Can not be empty!";
+                        } else {
+                          return null;
+                        }
+                      },
+                      onChanged: (val) {
+                        email = val;
+                      },
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      labelText: "Name",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    validator: (val) {
-                      if (val.isEmpty) {
-                        return "This Field can not be empty!";
-                      } else if (val.length < 6) {
-                        return "The length of the password can not be less than 6";
-                      } else {
-                        return null;
-                      }
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                        ),
-                      ),
-                      labelText: "Password",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    elevation: 20,
-                    color: Colors.white,
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 5, horizontal: 30),
-                      child: Text(
-                        "Sign In",
-                        style: TextStyle(
-                          fontFamily: 'HKGrotesk',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
+                        labelText: "Email",
                       ),
                     ),
-                    onPressed: () {},
-                  )
-                ],
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (val) {
+                        if (val.isEmpty) {
+                          return "This Field can not be empty!";
+                        } else if (val.length < 6) {
+                          return "The length of the password can not be less than 6";
+                        } else {
+                          return null;
+                        }
+                      },
+                      onChanged: (val) {
+                        password = val;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.vpn_key),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        labelText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 20,
+                      color: Colors.white,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(
+                            fontFamily: 'HKGrotesk',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        _formKey.currentState.validate();
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
           ),
